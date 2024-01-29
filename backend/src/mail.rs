b4eff951c0b0da97ac1b29d446b32bcd7146c7ed
Horváth Bálint <horvbalint99@gmail.com>
2024-01-29 16:07:30 +0100

    fix

--- backend/src/mail.rs
+++ backend/src/mail.rs
@@ -1,90 +1,125 @@
-use std::fs;
 use serde_json::{json, Value};
+use std::fs;
 
-use crate::{EnvVars, schemas::Feedback};
+use crate::{schemas::Feedback, EnvVars};
 
 #[cfg(debug_assertions)]
-pub async fn send_confirmation(_username: &str, _email: &str, token: &str, env_vars: &EnvVars) -> Result<(), String> {
-  println!("CONFIRM REGISTRATION: \n {}/confirm?token={token}", env_vars.fontend_url);
-  println!("CANCEL REGISTRATION: \n {}/cancel?token={token}", env_vars.fontend_url);
+pub async fn send_confirmation(
+    _username: &str,
+    _email: &str,
+    token: &str,
+    env_vars: &EnvVars,
+) -> Result<(), String> {
+    println!(
+        "CONFIRM REGISTRATION: \n {}/confirm?token={token}",
+        env_vars.frontend_url
+    );
+    println!(
+        "CANCEL REGISTRATION: \n {}/cancel?token={token}",
+        env_vars.frontend_url
+    );
 
-  Ok(())
+    Ok(())
 }
 
 #[cfg(not(debug_assertions))]
-pub async fn send_confirmation(username: &str, email: &str, token: &str, env_vars: &EnvVars) -> Result<(), String> {
-  let mut html = fs::read_to_string("emails/emailConfirmation.html")
-    .map_err(|_| "Failed to open email template".to_string())?;
+pub async fn send_confirmation(
+    username: &str,
+    email: &str,
+    token: &str,
+    env_vars: &EnvVars,
+) -> Result<(), String> {
+    let mut html = fs::read_to_string("emails/emailConfirmation.html")
+        .map_err(|_| "Failed to open email template".to_string())?;
 
-  html = html.replace("{{CONFIRM_URL}}", format!("{}/confirm?token={token}", env_vars.fontend_url).as_str());
-  html = html.replace("{{CANCEL_URL}}", format!("{}/cancel?token={token}", env_vars.fontend_url).as_str());
-  html = html.replace("{{USERNAME}}", &html_escape::encode_safe(username));
+    html = html.replace(
+        "{{CONFIRM_URL}}",
+        format!("{}/confirm?token={token}", env_vars.frontend_url).as_str(),
+    );
+    html = html.replace(
+        "{{CANCEL_URL}}",
+        format!("{}/cancel?token={token}", env_vars.frontend_url).as_str(),
+    );
+    html = html.replace("{{USERNAME}}", &html_escape::encode_safe(username));
 
-  let content = json!({
-    "Messages":[
-      {
-        "From": {
-          "Name": "Speer",
-          "Email": &env_vars.noreply_email,
-        },
-        "To": [{
-          "Name": &username,
-          "Email": &email,
-        }],
-        "Subject": "Speer - Confirm your email",
-        "HTMLPart": &html,
-      }
-    ]
-  });
+    let content = json!({
+      "Messages":[
+        {
+          "From": {
+            "Name": "Speer",
+            "Email": &env_vars.noreply_email,
+          },
+          "To": [{
+            "Name": &username,
+            "Email": &email,
+          }],
+          "Subject": "Speer - Confirm your email",
+          "HTMLPart": &html,
+        }
+      ]
+    });
 
-  send_email(content, env_vars).await
+    send_email(content, env_vars).await
 }
 
-pub async fn send_feedback_notification(feedback: &Feedback, env_vars: &EnvVars) -> Result<(), String> {
-  let mut html = fs::read_to_string("emails/feedbackNotification.html")
-    .map_err(|_| "Failed to open email template".to_string())?;
+pub async fn send_feedback_notification(
+    feedback: &Feedback,
+    env_vars: &EnvVars,
+) -> Result<(), String> {
+    let mut html = fs::read_to_string("emails/feedbackNotification.html")
+        .map_err(|_| "Failed to open email template".to_string())?;
 
-  html = html.replace("{{TYPE}}", &html_escape::encode_safe(&feedback.r#type));
-  html = html.replace("{{DATE}}", &html_escape::encode_safe(&feedback.date.to_string()));
-  html = html.replace("{{VERSION}}", &html_escape::encode_safe(&feedback.version));
-  html = html.replace("{{DESCRIPTION}}", &html_escape::encode_safe(&feedback.description));
+    html = html.replace("{{TYPE}}", &html_escape::encode_safe(&feedback.r#type));
+    html = html.replace(
+        "{{DATE}}",
+        &html_escape::encode_safe(&feedback.date.to_string()),
+    );
+    html = html.replace("{{VERSION}}", &html_escape::encode_safe(&feedback.version));
+    html = html.replace(
+        "{{DESCRIPTION}}",
+        &html_escape::encode_safe(&feedback.description),
+    );
 
-  let content = json!({
-    "Messages":[
-      {
-        "From": {
-          "Name": "Speer",
-          "Email": &env_vars.noreply_email,
-        },
-        "To": [{
-          "Name": "Admin",
-          "Email": &env_vars.admin_email,
-        }],
-        "Subject": "Speer - New feedback",
-        "HTMLPart": &html,
-      }
-    ]
-  });
+    let content = json!({
+      "Messages":[
+        {
+          "From": {
+            "Name": "Speer",
+            "Email": &env_vars.noreply_email,
+          },
+          "To": [{
+            "Name": "Admin",
+            "Email": &env_vars.admin_email,
+          }],
+          "Subject": "Speer - New feedback",
+          "HTMLPart": &html,
+        }
+      ]
+    });
 
-  send_email(content, env_vars).await
+    send_email(content, env_vars).await
 }
 
 async fn send_email(content: Value, env_vars: &EnvVars) -> Result<(), String> {
-  let client = reqwest::Client::new();
-  let response = client.post("https://api.mailjet.com/v3.1/send")
-    .basic_auth(env_vars.mailjet_public.as_str(), Some(env_vars.mailjet_secret.as_str()))
-    .json(&content)
-    .send()
-    .await
-    .or_else(|err| {
-      eprintln!("{:#?}", err);
-      Err("Failed to send email".to_string())
-    })?;
+    let client = reqwest::Client::new();
+    let response = client
+        .post("https://api.mailjet.com/v3.1/send")
+        .basic_auth(
+            env_vars.mailjet_public.as_str(),
+            Some(env_vars.mailjet_secret.as_str()),
+        )
+        .json(&content)
+        .send()
+        .await
+        .or_else(|err| {
+            eprintln!("{:#?}", err);
+            Err("Failed to send email".to_string())
+        })?;
 
-  if !response.status().is_success() {
-    eprintln!("{:#?}", response.status());
-    return Err(format!("Mailjet API error: {}", response.status()) );
-  }
+    if !response.status().is_success() {
+        eprintln!("{:#?}", response.status());
+        return Err(format!("Mailjet API error: {}", response.status()));
+    }
 
-  Ok(())
+    Ok(())
 }
